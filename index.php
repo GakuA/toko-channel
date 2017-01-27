64f1d6579df1e6f2d84f710970f691e4624d1999
赤澤学 <gaku@gaku-no-MacBook-Pro.local>
2017-01-27 22:53:24 +0900

    “db修正”

--- index.php
+++ index.php
@@ -33,15 +33,6 @@
 		}
 
 	</script>
-
-	<!-- head 内か、body 終了タグの直前に次のタグを貼り付けてください。 -->
-	<script src="https://apis.google.com/js/platform.js" async defer>
-		{
-			lang: 'ja'
-		}
-
-	</script>
-
 </head>
 
 <body>
