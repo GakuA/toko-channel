d58d694bb4fee7c87267d21fc1070d0e44e434fa
赤澤学 <gaku@gaku-no-MacBook-Pro.local>
2017-01-28 15:15:33 +0900

    “sql修正”

--- index.php
+++ index.php
@@ -128,7 +128,7 @@
 			<?php include "video.php"; ?>
 			<!--video-->
 
-			<?php mysql_close($link); ?>
+			<?php pg_close($link); ?>
 
 			<div style="clear:both;padding:6px 4px;border-top:2px solid #ff0000;width:100%;">
 				<!-- <a class="black" href="how.html">使い方</a> -->
