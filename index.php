daac340ecc9e06f76ad283d30f03292d4bfd9013
GakuA <akazawagaku@gmail.com>
2017-06-07 22:32:34 +0900

    rakuten off

--- index.php
+++ index.php
@@ -115,6 +115,25 @@
 				<span><a class="black" href="mailto:akazawagaku@gmail.com">お問い合わせ</a></span>
 			</div>
 		</div>
+		<div style="margin-left:1152px;">
+			<!-- Rakuten Widget FROM HERE -->
+			<script type="text/javascript">
+				rakuten_design = "slide";
+				rakuten_affiliateId = "142a8617.af4bd4b0.142a8618.1730d9f1";
+				rakuten_items = "ctsmatch";
+				rakuten_genreId = 0;
+				rakuten_size = "120x600";
+				rakuten_target = "_blank";
+				rakuten_theme = "gray";
+				rakuten_border = "on";
+				rakuten_auto_mode = "on";
+				rakuten_genre_title = "off";
+				rakuten_recommend = "on";
+
+			</script>
+			<script type="text/javascript" src="http://xml.affiliate.rakuten.co.jp/widget/js/rakuten_widget.js"></script>
+			<!-- Rakuten Widget TO HERE -->
+		</div>
 	</div>
 </body>
 
