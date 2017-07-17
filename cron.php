1127a09a7cd171aadc675e471298683545472780
GakuA <akazawagaku@gmail.com>
2017-07-17 10:18:08 +0900

    削除クーロン

--- cron.php
+++ cron.php
@@ -3,35 +3,30 @@
      include "function.inc";
 
 //mysql
-    $link = mysql_connect('mysql1.minibird.netowl.jp', 'tokoch_shuron', 'yamamototamura40');
+    $link = pg_connect('host=ec2-23-21-224-199.compute-1.amazonaws.com dbname=d8afnl5oh4vu8b user=mvurgftlbjcxfk password=42e63ae60ed1e92cc3a729d3d92e89c03f69e2b37c76ecb2253e0b8d02064d71');
     if (!$link) {
-        die('接続失敗です。'.mysql_error());
+        die('接続失敗です。');
     }
 
-    $db_selected = mysql_select_db('tokoch_video', $link);
-    if (!$db_selected){
-        die('データベース選択失敗です。'.mysql_error());
-    }
-    
-    $result = mysql_query("SELECT v FROM video");
+    $result = pg_query("SELECT v FROM video");
     if(!$result){
-        exit('SELECTクエリーが失敗しました。'.mysql_error());
+        exit('SELECTクエリーが失敗しました。');
     }
 
-    while($row = mysql_fetch_assoc($result)){
+    while($row = pg_fetch_assoc($result)){
         $vvv = $row['v'];
         if(getPageTitle('https://www.youtube.com/watch?v=' . $vvv) == "YouTube"){
-            $result_flag = mysql_query("DELETE FROM video WHERE v = '$vvv'");
+            $result_flag = pg_query("DELETE FROM video WHERE v = '$vvv'");
 
             if (!$result_flag) {
-                die('DELETEクエリーが失敗しました。'.mysql_error());
+                die('DELETEクエリーが失敗しました。');
             }
-            
+
             while(@unlink("comment/yamamototamura_".$vvv."/comment.dat")){
             }
             while(@rmdir("comment/yamamototamura_".$vvv)){
             }
         }
     }
-    
+
     mysql_close($link);
