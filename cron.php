fd0b86054c5d8202afb614de6a69e3927215fabd
GakuA <akazawagaku@gmail.com>
2017-07-17 16:05:23 +0900

    test

--- cron.php
+++ cron.php
@@ -8,7 +8,7 @@
         die('接続失敗です。');
     }
 
-    $result = pg_query("SELECT v FROM video limit 50 offset 1100");
+    $result = pg_query("SELECT v FROM video limit 50 offset 1150");
     if(!$result){
         exit('SELECTクエリーが失敗しました。');
     }
