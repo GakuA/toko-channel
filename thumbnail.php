006439a219ad45b3df9ce8751c8cb934e8d9b29d
GakuA <akazawagaku@gmail.com>
2017-09-02 16:14:49 +0900

    test

--- thumbnail.php
+++ thumbnail.php
@@ -49,7 +49,7 @@
             $total = "pow($total, 1/".count($_SESSION["rank"]).")";
 
             $result = pg_query("SELECT *, $total/(total) as rank FROM video where $total != 0 order by rank desc,time desc");
-var_dump($result);
+var_dump($_SESSION["tab"]);
             if(!$result){
                 exit('SELECTクエリーが失敗しました。');
             }
