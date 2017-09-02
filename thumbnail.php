3f5c11c11b02433a7392b0731e01a0c0a17504b0
GakuA <akazawagaku@gmail.com>
2017-09-03 04:20:59 +0900

    test

--- thumbnail.php
+++ thumbnail.php
@@ -44,7 +44,7 @@
             $total = 0;
             foreach($_SESSION["tabList"][$_SESSION["tab"]] as $value){
                 $total .= " + ".$value;
-                $zero .= ($total == 0 ? "," : "") . $value . " != 0";
+                $zero .= ($total != 0 ? ", " : "") . $value . " != 0";
             }
 
             $total = "pow($total, 1 / ".count($_SESSION["rank"]).")";
