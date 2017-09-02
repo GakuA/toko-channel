6acbd10997b54295a97477b49663f9cc7b75839a
GakuA <akazawagaku@gmail.com>
2017-09-03 07:14:31 +0900

    test

--- thumbnail.php
+++ thumbnail.php
@@ -51,7 +51,7 @@
             }
 
             $total = "($total) / ".count($_SESSION["rank"]);
-//var_dump($zero);
+var_dump($total);
             $result = pg_query("SELECT *, (($total) / total) as rank FROM video where $zero order by rank desc, time desc");
 
             if(!$result){
