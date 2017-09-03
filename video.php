2e5bc78e6dd1c9d999127efac08cd776103bfc9e
GakuA <akazawagaku@gmail.com>
2017-09-03 11:40:33 +0900

    test

--- video.php
+++ video.php
@@ -204,7 +204,7 @@
 
     $v = $_SESSION["v"];
     $cv = $_COOKIE["$v"];
-    echo "<span style=\"position:absolute; top:274px\"><input type=\"button\" value=\"評価する\" name=\"go\" style=\"cursor:pointer\" onclick=\"check_hyoka('')\"><img onmouseover=\"qaHyoka.style.display='block'\" onmouseout=\"qaHyoka.style.display='none'\" src=\"img/hatena.png\" style=\"width:15px;margin-left:5px\"></span><br>\n";
+    echo "<span style=\"position:absolute; top:274px\"><input type=\"button\" value=\"評価する\" name=\"go\" style=\"cursor:pointer\" onclick=\"check_hyoka('$cv')\"><img onmouseover=\"qaHyoka.style.display='block'\" onmouseout=\"qaHyoka.style.display='none'\" src=\"img/hatena.png\" style=\"width:15px;margin-left:5px\"></span><br>\n";
     echo "<div style=\"position:absolute; top:300px\">評価回数：".$_SESSION["total"]."</div>\n";
     echo "</form>\n";
     echo "</div>\n";
